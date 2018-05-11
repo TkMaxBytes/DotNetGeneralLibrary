79ea56046d65c97deace8e09ecc37041a214b8f8
Terrence Knoesen <Terrence.Knoesen@motorolasolutions.com>
2018-05-11 14:53:02 +0100

    Made some changes to the snippets and added some code to the new DotNetUtils library.

---
+++ DotNetUtils/GeneralUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+
+namespace DotNetUtils
+{
+    public static class GeneralUtils
+    {
+        public static string GetApplicationName(int intReturnType)
+        {
+            string strVer;
+
+            Assembly asm = Assembly.GetExecutingAssembly();
+            AssemblyName asmName = asm.GetName();
+            Version objVer = asmName.Version;
+
+
+            string strName = asmName.Name;
+            switch (intReturnType)
+            {
+                case 0:
+                    return strName;
+                case 1:
+                    strVer = String.Format("{0} - {1}.{2}.{3}.{4}", strName, objVer.Major, objVer.Minor, objVer.Build, objVer.Revision);
+                    return strVer;
+                default:
+                    return strName;
+            }
+        }
+
+
+    }
+}
